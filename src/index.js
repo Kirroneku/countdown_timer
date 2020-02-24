88e79d5da1efe3d8e6ad72382d6c345604caa770
Jialin Shan <chansukeizumi@gmail.com>
2020-02-24 16:20:13 -0500

    intialize redux on the project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />  
+    </Provider>
+   ,
     document.getElementById('root')
 );
